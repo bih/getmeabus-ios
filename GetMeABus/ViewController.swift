40dcb4b5715da80fb8724fa5ce3c45c33bcc09cd
Bilawal Hameed <bilawal@studenthack.com>
2015-08-01 16:07:44 +0100

    Initial Commit

---
+++ GetMeABus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetMeABus
+//
+//  Created by Bilawal Hameed on 01/08/2015.
+//  Copyright (c) 2015 SyeefOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
